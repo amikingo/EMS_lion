3238032e63884f1c0892e445298dc5c30df0ae96
BlackPhinix <abenrahel@gmail.com>
2023-03-26 13:33:25 +0300

    abena

--- customer/header.php
+++ customer/header.php
@@ -1472,7 +1472,7 @@ section {
       <nav class="nav-menu d-none d-lg-block"style="margin-left: 500px;">
         <ul>
           <li class="active"><a href="index.php">Home</a></li>
-          <li><a href="About us.php">About us</a></li>         
+          <li><a href="About us.php">About-us</a></li>         
           <li><a href="contact us.php">Contact</a></li>
           <li><a href="index.php">Hiring Form</a></li>
           <li><a href="search-request.php">Request Status</a>
