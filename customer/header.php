3d3266dbcffcd4b3ae397637321187a2fbc7807b
amiKingo <amikingo201@gmail.com>
2023-03-26 13:21:10 +0300

    change

--- customer/header.php
+++ customer/header.php
@@ -1,18 +1,30 @@
-  <header class="header-section">
-    
-    <div class="header-bottom">
-      <a href="index.php" class="site-logo">
-        <h4 style="color: white"><strong>Lion</strong></h4>
-      </a>
-    
-      <div class="container">
-        <ul class="main-menu">
-         
+<!DOCTYPE html>
+<head>
+<style>
+
+
+</style>
+</head>
+   <!-- ======= Header ======= -->
+   <header id="header" class="fixed-top">
+    <div class="container d-flex align-items-center">
+      <a class="navbar-brand" href="index.php"></a>
+   
+      <img alt="logo" src="assets/img/LOGO.png" style="width: 800px;height: 50px;">
+      
+
+      <nav class="nav-menu d-none d-lg-block"style="margin-left: 500px;">
+        <ul>
+          <li class="active"><a href="index.php">Home</a></li>
+          <li><a href="About us.php">About us</a></li>         
+          <li><a href="contact us.php">Contact</a></li>
           <li><a href="index.php">Hiring Form</a></li>
           <li><a href="search-request.php">Request Status</a>
                      </li>
- 
+          
+          </li>
+
         </ul>
-      </div>
+      </nav>
     </div>
   </header>
