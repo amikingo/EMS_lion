2fd6555c1d3ab03753f17d30a3583fb736951a78
amiKingo <amikingo201@gmail.com>
2023-03-26 13:30:11 +0300

    add

--- customer/header.php
+++ customer/header.php
@@ -1,10 +1,38 @@
-<!DOCTYPE html>
-<head>
 <style>
+#header {
+    background: #fff;
+    transition: all 0.5s;
+    z-index: 997;
+    padding: 15px 0;
+    box-shadow: 0px 0 18px rgba(55, 66, 59, 0.08);
+}
 
+#header .logo {
+    font-size: 32px;
+    margin: 0;
+    padding: 0;
+    line-height: 1;
+    font-weight: 600;
+    letter-spacing: 1px;
+    text-transform: uppercase;
+    font-family: "Poppins", sans-serif;
+}
+
+#header .logo a {
+    color: #007bff !important;
+}
+
+#header .logo img {
+    max-height: 40px;
+}
+
+@media (max-width: 992px) {
+    #header .logo {
+        font-size: 28px;
+    }
+}
 
 </style>
-</head>
    <!-- ======= Header ======= -->
    <header id="header" class="fixed-top">
     <div class="container d-flex align-items-center">
