1f4148e68cfc05336c25f4e6506084b2dea1d360
amiKingo <amikingo201@gmail.com>
2023-03-26 19:37:31 +0300

    add

--- customer/header.php
+++ customer/header.php
@@ -318,15 +318,12 @@ h6 {
     <div class="container d-flex align-items-center">
       <a class="navbar-brand" href="index.php"></a>
    
-      <img alt="logo" src="../assets/img/LOGO.png" style="width: 800px;height: 50px;">
+      <img alt="logo" src="../assets/img/LOGO.png" style="width: 350px;height: 50px;">
       
 
       <nav class="nav-menu d-none d-lg-block"style="margin-left: 500px;">
         <ul>
           <li class="active"><a href="index.php">Home</a></li>
-          <li><a href="About us.php">About us</a></li>         
-          <li><a href="contact us.php">Contact</a></li>
-          <li><a href="index.php">Hiring Form</a></li>
           <li><a href="search-request.php">Request Status</a>
                      </li>
           
