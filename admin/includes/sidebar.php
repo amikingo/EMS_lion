e5d3585e8a453828626e6eb77c33dd7fd61b7a8b
amiKingo <amikingo201@gmail.com>
2023-03-31 21:24:12 +0300

    admin setting change

--- admin/includes/sidebar.php
+++ admin/includes/sidebar.php
@@ -38,36 +38,7 @@ foreach($results as $row)
         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
           <!-- Add icons to the links using the .nav-icon class
                with font-awesome or any other icon font library -->
-            <li class="nav-item has-treeview">
-            <a href="#" class="nav-link">
-              <i class="nav-icon fas fa-plus-square"></i>
-              <p>
-                Admin Setting
-                <i class="fas fa-angle-left right"></i>
-                
-              </p>
-            </a>
-            <ul class="nav nav-treeview">
-              <li class="nav-item">
-                <a href="admin-profile.php" class="nav-link">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>Profile Update</p>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a href="change-password.php" class="nav-link">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>Change Password</p>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a href="logout.php" class="nav-link">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>Logout</p>
-                </a>
-              </li>
-             </ul>
-          </li>   
+        
           <li class="nav-item">
             <a href="dashboard.php" class="nav-link">
               <i class="nav-icon fas fa-tachometer-alt"></i>
@@ -158,6 +129,36 @@ foreach($results as $row)
             </a>
         
           </li>
+          <li class="nav-item has-treeview">
+            <a href="#" class="nav-link">
+              <i class="nav-icon fas fa-plus-square"></i>
+              <p>
+                Admin Setting
+                <i class="fas fa-angle-left right"></i>
+                
+              </p>
+            </a>
+            <ul class="nav nav-treeview">
+              <li class="nav-item">
+                <a href="admin-profile.php" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Profile Update</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="change-password.php" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Change Password</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="logout.php" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Logout</p>
+                </a>
+              </li>
+             </ul>
+          </li> 
         </ul>
       </nav>
       <!-- /.sidebar-menu -->
