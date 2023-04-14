570d5be22e69e9184c099b5521bb4d27c01fa438
amiKingo <amikingo201@gmail.com>
2023-04-14 08:54:58 +0300

    attendance add

---
+++ attendance-php/ClassTeacher/Includes/footer.php
@@ -0,0 +1,9 @@
+<footer class="sticky-footer bg-white">
+        <div class="container my-auto">
+          <div class="copyright text-center my-auto">
+            <span> &copy; <script> document.write(new Date().getFullYear()); </script> - Developed by NATIONAL DIPLOMA
+              STUDENT ATTENDANCE MANAGEMENT SYSTEM
+            </span>
+          </div>
+        </div>
+      </footer>
