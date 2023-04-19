51b5610e3dd20e3932361094c82445877b63bc49
amiKingo <amikingo201@gmail.com>
2023-04-19 07:17:30 +0300

    teacher log in change

--- attendance-php/index123.php
+++ attendance-php/index123.php
@@ -22,7 +22,7 @@ session_start();
 
 </head>
 
-<body class="bg-gradient-login">
+<body class="bg-gradient-login" style="background-image: url('img/logo/loral1.jpe00g');">
   <!-- Login Content -->
   <div class="container-login">
     <div class="row justify-content-center">
@@ -32,12 +32,20 @@ session_start();
             <div class="row">
               <div class="col-lg-12">
                 <div class="login-form">
+                <h5 align="center">STUDENT ATTENDANCE SYSTEM</h5>
                   <div class="text-center">
                     <img src="img/logo/attnlg.jpg" style="width:100px;height:100px">
                     <br><br>
-                    <h1 class="h4 text-gray-900 mb-4">Login</h1>
+                    <h1 class="h4 text-gray-900 mb-4">Admin Login Panel</h1>
                   </div>
                   <form class="user" method="Post" action="">
+                  <div class="form-group">
+                  <select required name="userType" class="form-control mb-3">
+                          <option value="">--Select User Roles--</option>
+                          <option value="Administrator">Administrator</option>
+                          <option value="ClassTeacher">ClassTeacher</option>
+                        </select>
+                    </div>
                     <div class="form-group">
                       <input type="text" class="form-control" required name="username" id="exampleInputEmail" placeholder="Enter Email Address">
                     </div>
@@ -52,7 +60,7 @@ session_start();
                       </div>
                     </div>
                     <div class="form-group">
-                        <input type="submit"  class="btn btn-primary btn-block" value="Login" name="login" />
+                        <input type="submit"  class="btn btn-success btn-block" value="Login" name="login" />
                     </div>
                      </form>
 
@@ -60,38 +68,75 @@ session_start();
 
   if(isset($_POST['login'])){
 
+    $userType = $_POST['userType'];
     $username = $_POST['username'];
     $password = $_POST['password'];
-
     $password = md5($password);
 
-    $query = "SELECT * FROM tblclassteacher WHERE emailAddress = '$username' AND password = '$password'";
-    $rs = $conn->query($query);
-    $num = $rs->num_rows;
-    $rows = $rs->fetch_assoc();
+    if($userType == "Administrator"){
 
-    if($num > 0){
+      $query = "SELECT * FROM tbladmin WHERE emailAddress = '$username' AND password = '$password'";
+      $rs = $conn->query($query);
+      $num = $rs->num_rows;
+      $rows = $rs->fetch_assoc();
 
-      $_SESSION['userId'] = $rows['Id'];
-      $_SESSION['firstName'] = $rows['firstName'];
-      $_SESSION['lastName'] = $rows['lastName'];
-      $_SESSION['emailAddress'] = $rows['emailAddress'];
-      $_SESSION['classId'] = $rows['classId'];
-      $_SESSION['classArmId'] = $rows['classArmId'];
+      if($num > 0){
 
-      echo "<script type = \"text/javascript\">
-      window.location = (\"ClassTeacher/index.php\")
-      </script>";
+        $_SESSION['userId'] = $rows['Id'];
+        $_SESSION['firstName'] = $rows['firstName'];
+        $_SESSION['lastName'] = $rows['lastName'];
+        $_SESSION['emailAddress'] = $rows['emailAddress'];
+
+        echo "<script type = \"text/javascript\">
+        window.location = (\"Admin/index.php\")
+        </script>";
+      }
+
+      else{
+
+        echo "<div class='alert alert-danger' role='alert'>
+        Invalid Username/Password!
+        </div>";
+
+      }
     }
+    else if($userType == "ClassTeacher"){
 
+      $query = "SELECT * FROM tblclassteacher WHERE emailAddress = '$username' AND password = '$password'";
+      $rs = $conn->query($query);
+      $num = $rs->num_rows;
+      $rows = $rs->fetch_assoc();
+
+      if($num > 0){
+
+        $_SESSION['userId'] = $rows['Id'];
+        $_SESSION['firstName'] = $rows['firstName'];
+        $_SESSION['lastName'] = $rows['lastName'];
+        $_SESSION['emailAddress'] = $rows['emailAddress'];
+        $_SESSION['classId'] = $rows['classId'];
+        $_SESSION['classArmId'] = $rows['classArmId'];
+
+        echo "<script type = \"text/javascript\">
+        window.location = (\"ClassTeacher/index.php\")
+        </script>";
+      }
+
+      else{
+
+        echo "<div class='alert alert-danger' role='alert'>
+        Invalid Username/Password!
+        </div>";
+
+      }
+    }
     else{
 
-      echo "<div class='alert alert-danger' role='alert'>
-      Invalid Username/Password!
-      </div>";
+        echo "<div class='alert alert-danger' role='alert'>
+        Invalid Username/Password!
+        </div>";
 
     }
-  }
+}
 ?>
 
                     <!-- <hr>
@@ -101,13 +146,8 @@ session_start();
                     <a href="index.html" class="btn btn-facebook btn-block">
                       <i class="fab fa-facebook-f fa-fw"></i> Login with Facebook
                     </a> -->
-                  <hr>
-                  <div class="text-center">
-                    <a class="font-weight-bold small" href="classTeacherLogin.php">Class Teacher Login!</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                    <!-- <a class="font-weight-bold small" href=".php">Cooperative Account!</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                    <a class="font-weight-bold small" href="forgotPassword.php">Forgot Password?</a> -->
 
-                  </div>
+                
                   <div class="text-center">
                   </div>
                 </div>
