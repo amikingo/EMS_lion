3ba74c8d027f252a8efac34e1208837e209eb008
amiKingo <amikingo201@gmail.com>
2023-05-19 22:08:14 +0300

    aa

--- admin/index.php
+++ admin/index.php
@@ -98,7 +98,7 @@ background-color:#bfbfbf;
                     <div class="form-group">
                       <input type="password" class="form-control" placeholder="Password" name="password" required="true" value="<?php if(isset($_COOKIE["userpassword"])) { echo $_COOKIE["userpassword"]; } ?>">
                     </div>
-    <div><a href="forgot-password.php" style="margin-left: 50%;">Forgot-password ?</a></div>
+<div><a href="forgot-password.php" style="margin-left: 45%;">Forgot-password ?</a></div>
                     <div class="form-group">
                       <div class="icheck-primary">
               <input type="checkbox" id="remember" name="remember" <?php if(isset($_COOKIE["user_login"])) { ?> checked <?php } ?> />
