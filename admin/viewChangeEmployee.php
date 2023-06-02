a3c782f08703b0c3d51b83492d4ae2b1b2759255
BlackPhinix <abenrahel@gmail.com>
2023-06-02 19:42:13 +0300

    emp change

---
+++ admin/viewChangeEmployee.php
@@ -0,0 +1,71 @@
+<!-- <?php
+//include('includes/dbconnection.php');
+?> -->
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Lion Security Services | View More</title>
+  <link href="dist/img/fav.png" rel="icon">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
+  <!-- Ionicons -->
+  <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="dist/css/adminlte.min.css">
+  <!-- Google Font: Source Sans Pro -->
+  <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
+</head>
+<body class="hold-transition sidebar-mini">
+<div class="wrapper">
+  <?php include_once('includes/header.php');?>
+  <?php include_once('includes/sidebar.php');?>
+
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1>View More About Trainee</h1>
+          </div>
+          <div class="col-sm-6">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item"><a href="trainerLists.php">Home</a></li>
+              <li class="breadcrumb-item active">View More About Trainee</li>
+            </ol>
+          </div>
+        </div>
+      </div><!-- /.container-fluid -->
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+      <div class="container-fluid">
+        <div class="row">
+          <!-- left column -->
+          <div class="col-md-12">
+            <!-- general form elements -->
+           
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+
+  <?php include_once('includes/footer.php');?>
+</div>
+
+<!-- jQuery -->
+<script src="plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- AdminLTE App -->
+<script src="dist/js/adminlte.min.js"></script>
+<script>
+  $(function () {
+    $("#example1").DataTable();
+  });
+</script>
+</body>
+</html>
