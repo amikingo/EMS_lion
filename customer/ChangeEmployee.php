460776c3e8b00f6bb328cb197678bdb97980e4bd
amiKingo <amikingo201@gmail.com>
2023-06-05 10:24:22 +0300

    change in customer still in progres

--- customer/ChangeEmployee.php
+++ customer/ChangeEmployee.php
@@ -1,63 +1,23 @@
 <?php
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
 include('dbconnection.php');
 session_start();
-
-
-if(isset($_POST['submit'])) {
-    // Get the selected guard and remark from the form
-    $guardId = $_POST['gender'];
-    $remark = $_POST['add'];
-
-    // Fetch the guard name based on the selected guardId
-    $sql = "SELECT Name FROM tblguard WHERE ID = :guardId";
-    $query = $dbh->prepare($sql);
-    $query->bindParam(':guardId', $guardId, PDO::PARAM_STR);
-    $query->execute();
-    $guard = $query->fetch(PDO::FETCH_ASSOC);
-    $guardName = $guard['Name'];
-
-    // Get the current session's companyName
-    $companyName = $_SESSION['user_id'];
-
-    // Combine the guard name and remark as comma-separated values
-    $data = $guardName . ', ' . $remark;
-
-    // Update the data in the database for the current session's companyName
-    $sql = "UPDATE tblhiring SET data = CONCAT(data, ', ', :data) WHERE companyName = :companyName";
-    $query = $dbh->prepare($sql);
-    $query->bindParam(':data', $data, PDO::PARAM_STR);
-    $query->bindParam(':companyName', $companyName, PDO::PARAM_STR);
-    $query->execute();
-
-    // Check if the update was successful
-    if($query) {
-        $statusMsg = "Data updated successfully!";
-        $alertStyle = "alert alert-success";
-    } else {
-        $statusMsg = "Error updating data!";
-        $alertStyle = "alert alert-danger";
-    }
-}
-
+//error_reporting(0);
+ $user_id = $_SESSION["user_id"];
 ?>
 <!DOCTYPE html>
 <html lang="zxx">
 <head>
-	<title>lion security services |Request Guard</title>
-	<link href="assets/img/fav.png" rel="icon">	
+<link href="assets/img/fav.png" rel="icon">
+	<title>lion security services |Search Request</title>	
 	<link rel="stylesheet" href="css/bootstrap.min.css"/>
 	<link rel="stylesheet" href="css/font-awesome.min.css"/>
 	<link rel="stylesheet" href="css/owl.carousel.min.css"/>
 	<link rel="stylesheet" href="css/nice-select.css"/>
 	<link rel="stylesheet" href="css/slicknav.min.css"/>
-	<!--<link href="../assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet"> -->
 	<link href="../assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
 	<!-- Main Stylesheets -->
 	<link rel="stylesheet" href="css/style.css"/>
 	<link rel="stylesheet" href="css/header.css"/>
-	
 </head>
 <body>
 	<!-- Page Preloder -->
@@ -71,7 +31,7 @@ if(isset($_POST['submit'])) {
     <div class="container d-flex align-items-center">
       <a class="navbar-brand" href="index.php"></a>
    
-	  <img alt="logo" src="../assets/img/LOGO.png" style="width: 800px;height: 50px;">
+      <img alt="logo" src="../assets/img/LOGO.png" style="width: 800px;height: 50px;">
       
 
       <nav class="nav-menu d-none d-lg-block"style="margin-left: 400px;">
@@ -82,9 +42,9 @@ if(isset($_POST['submit'])) {
   <li class="drop-down">
     <button type="button" class="btn btn-primary">Request</button>
     <ul class="submenu">
-      <li ><a href="index.php">Request Employee</a></li>
-      <li><a href="search-request.php">Check Request</a></li>
-	  <li class="active"><a href="ChangeEmployee.php">Change Security</a></li>
+      <li><a href="index.php">Request Employee</a></li>
+      <li ><a href="search-request.php">Check Request</a></li>
+  <li class="active"><a href="ChangeEmployee.php">Change Security</a></li>
     </ul>
   </li>
   <li class="drop-down">
@@ -94,125 +54,114 @@ if(isset($_POST['submit'])) {
       <li><a href="login/change-password.php">Change Password</a></li>
       <li><a href="login/logout.php">Logout</a></li>
     </ul>
+
   </li>
 </ul>
       </nav>
     </div>
   </header>
-	<?php //include_once('header.php');?>
-	<section class="page-top-section set-bg" data-setbg="">
-		
+	
+	<section class="page-top-section set-bg" data-setbg="img/page-top-bg.jpg">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-7 m-auto text-white">
+					<h2>Search Request</h2>
+				</div>
+			</div>
+		</div>
 	</section>
 	<!-- Page top Section end -->
 
 	<!-- Contact Section -->
 	<section class="contact-page-section spad overflow-hidden">
 		<div class="container">
-			
-			<div class="row">
-				<div class="col-lg-8">
-					<form class="singup-form contact-form" method="post">
-					 <div class="<?php echo $alertStyle;?>" role="alert"><?php echo $statusMsg;?></div>
-						<?php	
-						$aid=$_SESSION['user_id'];
-						$sql="SELECT * from users Where id=:aid";
-						$query = $dbh -> prepare($sql);
-						$query->bindParam(':aid',$aid,PDO::PARAM_STR);
-						$query->execute();
-						$results=$query->fetchAll(PDO::FETCH_OBJ);
-						$cnt=1;
-						if($query->rowCount() > 0)
-							{
-foreach($results as $row)
-{               ?>
-								<div class="col-md-6">
-								<label style="padding-bottom: 10px;">Company Name</label>
-								<input type="text" placeholder="Phone Number" class="form-control" name="companyName" required="true" maxlength="10" pattern="[0-9]+"  value="<?php  echo $row->companyName;?>"readonly>
-								
-							</div>
-							  
-<div class="col-md-6">
-    <label style="padding-top: 10px;">Booking Number</label>
-    <select name="bookingNumber" required="true" class="form-control">
-        <option value="">Choose Booking Number</option>
-        <?php
-        include('dbconnection.php');
-        // Fetch the booking numbers for the current companyName
-        $companyName = $row->companyName; // Assuming $row->companyName is available
-        $sql = "SELECT * FROM tblhiring WHERE companyName = :companyName";
-        $query = $dbh->prepare($sql);
-        $query->bindParam(':companyName', $companyName, PDO::PARAM_STR);
-        $query->execute();
-        $bookings = $query->fetchAll(PDO::FETCH_ASSOC);
-
-        // Loop through the bookings and create the options
-        foreach ($bookings as $booking) {
-            $bookingNumberValue = $booking['BookingNumber'];
-        ?>
-        <option value="<?php echo $bookingNumberValue; ?>"><?php echo $bookingNumberValue; ?></option>
-        <?php
-        }
-      }
-        ?>
-    </select>
-</div>
-
-<div class="col-md-6">
-    <label style="padding-top: 10px;">Guard Name</label>
-    <select name="guardName" required="true" class="form-control">
-        <option value="">Choose Guard</option>
-        <?php
-        include('dbconnection.php');
-        // Fetch the guard IDs from tblhiring based on the selected booking number
-        if(isset($_POST['bookingNumber'])){
-            $selectedBookingNumber = $_POST['bookingNumber'];
-            $sql = "SELECT GuardAssign FROM tblhiring WHERE BookingNumber = :selectedBookingNumber";
-            $query = $dbh->prepare($sql);
-            $query->bindParam(':selectedBookingNumber', $selectedBookingNumber, PDO::PARAM_STR);
-            $query->execute();
-            $row = $query->fetch(PDO::FETCH_ASSOC);
-
-            if($row){
-                // Fetch the guards from tblguard where isAssigned = 1 and ID is in the parsed GuardAssign
-                $guardIds = explode(',', $row['GuardAssign']);
-                $placeholders = implode(',', array_fill(0, count($guardIds), '?'));
-                $sql = "SELECT * FROM tblguard WHERE isAssigned = 1 AND ID IN ($placeholders)";
-                $query = $dbh->prepare($sql);
-                $query->execute($guardIds);
-                $guards = $query->fetchAll(PDO::FETCH_ASSOC);
-
-                // Loop through the guards and create the options
-                foreach ($guards as $guard) {
-                    $guardId = $guard['ID'];
-                    $guardName = $guard['Name'];
-                ?>
-                <option value="<?php echo $guardId; ?>"><?php echo $guardName; ?></option>
-                <?php
-                }
-            }
-        }
+<div class="form-body">
+	<br><br><br>
+	<form role="form" method="post" name="printReport" action="">
 
 
-        ?>
-    </select>
-</div>
+  
+                <table id="example1" class="table table-bordered table-striped">
+                  <thead>
+                  <tr>
+                    <th>S.No</th>
+                    <th>Booking Number</th>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Contact Number</th>
+                    <th>Company Name</th>
+					<th>Remark</th>
+                    <th>Guard Ids</th>
+                <th>Remark for the change of Guard</th>
+    <th>Change Guard</th>
+                    
+
+                    
+                  </tr>
+                  </thead>
+                  <tbody>
+      <?php
+$aid = $_SESSION['user_id'];
+
+//$companyName = $_POST['companyName'];
+
+$mema = "SELECT companyName FROM users WHERE id=:aid";
+$mema_query = $dbh->prepare($mema);
+$mema_query->bindParam(':aid', $aid, PDO::PARAM_STR);
+$mema_query->execute();
+$mema_result = $mema_query->fetch(PDO::FETCH_OBJ);
+$companyName = $mema_result->companyName;
+
+$sql = "SELECT * FROM tblhiring WHERE  companyName=:companyName ";
+$query = $dbh->prepare($sql);
+$query->bindParam(':companyName', $companyName, PDO::PARAM_STR);
+$query->execute();
+$results = $query->fetchAll(PDO::FETCH_OBJ);
+
+$cnt = 1;
+if ($query->rowCount() > 0) {
+    foreach ($results as $row) {
+?>
+        <tr>
+            <td><?php echo htmlentities($cnt); ?></td>
+            <td><?php echo htmlentities($row->BookingNumber); ?></td>
+            <td><?php echo htmlentities($row->FirstName); ?> <?php echo htmlentities($row->LastName); ?></td>
+            <td><?php echo htmlentities($row->Email); ?></td>
+            <td><?php echo htmlentities($row->MobileNumber); ?></td>
+            <td><?php echo htmlentities($row->companyName); ?></td>
+			<td><?php echo htmlentities($row->Remark); ?></td>
+
+            <td><?php echo htmlentities($row->GuardAssign)?></td>
+<td><textarea name="data" rows="5" cols="50"></textarea></td>
+<td><button name="submit" type="submit" class="btn btn-primary">Update</button></td>
+
+           
+        </tr>
+        <input type="hidden" name="Guards" value="<?php echo htmlentities($row->GuardAssign);?>">
+<?php
+        $cnt = $cnt + 1;
+    }
+}
+else {
+?>
+    <tr>
+        <td colspan="8"> No record found against this search</td>
+    </tr>
 
 
-							<br>
-							<div class="col-md-12">
-					<label style="padding-top: 10px;">comment (Remark)</label>
-								<textarea placeholder="Enter comment(Remark)" name="add" required="true"></textarea>
-								 <input type="submit" class="btn btn-primary" value="send" name="submit">
-							</div>
-						</div>
-					</form>
-				</div>
+  <?php } ?>
+                 
+
+                </table>
+            </form>
+              </div>
+
 			</div>
 		</div>
 	</section>
 	<!-- Trainers Section end -->
 
-	
+	<br><br>
 	<!-- Footer Section -->
 	<footer id="footer">
 
